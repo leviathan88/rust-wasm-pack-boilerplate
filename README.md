42532a6d18553ab16cdf52f29b28cc86bc1c9de2
leviathanbeak88 <elvisdedic@outlook.com>
2019-10-09 18:14:24 +0200

    git init

---
+++ README.md
@@ -0,0 +1,15 @@
+### Rust Wasm boilerplate
+
+```
+To start:
+$ wasm-pack build
+
+[run the React boilerplate](https://github.com/leviathan88/rrr-webassembly)
+
+and connect it to pkg folder that you get from wasm-pack
+```
+
+```
+There is a lot of stuff added in Cargo.toml
+remove what you don't need and add what you need :)
+```
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use wasm_bindgen::prelude::*;
+extern crate web_sys;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
